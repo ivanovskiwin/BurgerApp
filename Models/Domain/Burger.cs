2e892ff0d85781f2847e7fe327f4e3cd288e7fd6
ivanovskiwin <ivanovskiwin@gmail.com>
2022-06-23 15:05:31 +0200

    initial

---
+++ Models/Domain/Burger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BurgerApp.Models.Domain
+{
+    public class Burger
+    {
+        public string Name { get; set; } = String.Empty;
+        public int Price { get; set; }
+        public bool IsVegetarian { get; set; } = false;
+
+        public bool IsVegan { get; set; } = false;
+
+        public bool HasFries { get; set; } = false;
+    }
+}
---
+++ Models/Domain/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BurgerApp.Models.Domain
+{
+    public class Order
+    {
+        public string FullName { get; set; } = String.Empty;
+
+        public string Address { get; set; } = String.Empty;
+
+        public bool IsDelivered { get; set; } = false;
+
+        public IEnumerable<Burger> Burgers { get; set; }
+
+        public string Location { get; set; } = String.Empty;
+    }
+}
